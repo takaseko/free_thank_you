ebdb1f17bba3fcfd2790b81a55226987973ae273
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-12 07:01:06 +0900

    usersとpostsのコントローラとビューファイルを作成

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -3,4 +3,9 @@ Rails.application.config.generators do |g|
   g.assets true
   g.stylesheets true
   g.helper false
+  g.test_framework :rspec,    # RSpecを使用
+    controller_specs: false,  # controller specは作らない
+    view_specs: false,        # view specは作らない
+    helper_specs: false,      # helper specは作らない
+    routing_specs: false      # routing specは作らない
 end
