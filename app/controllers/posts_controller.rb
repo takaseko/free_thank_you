b2dfef4217e4a1ebec28a374fb00fc9b3d73f564
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-14 06:01:26 +0900

    詳細機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,17 @@ class PostsController < ApplicationController
   def index
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
 
   def create
-    post = Post.create!(post_paramas)
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   private
