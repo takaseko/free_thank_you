903c77d6eee211a097832325423a118bd915bcd3
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-16 16:42:42 +0900

    ブートストラップで必要なJavaScript jQueryを使うための追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,5 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// JavaScript のファイルを読み込めるように追記
+require("bootstrap/dist/js/bootstrap")
